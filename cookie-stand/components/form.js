74f2cd299a121ce1649d167efa0ebea8ba8143db
miriamsilva <miriam-silva@live.com>
2021-09-01 13:06:33 -0700

    added tailwind

--- cookie-stand/components/form.js
+++ cookie-stand/components/form.js
@@ -1,8 +1,9 @@
 function Form(){
     return(
         <>
+        <div className = "w-full max-w-xs">
         <h2>Create Cookie Stand </h2>
-        <form>
+        <form className = "bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
             <label>Create Cookie Stand </label>
             <label>Location</label>
             <input type = "text" placeholder="location"/>
@@ -13,8 +14,8 @@ function Form(){
             <label>Average Cookies per Sale</label>
             <input type = "number" placeholder="Avg"/>
             <button>Create</button>
-            
         </form>
+        </div>
         </>
     )
 }
